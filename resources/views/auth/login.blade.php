688579dd90db7aeb3d8fea20ea5464ba4a4e7a5c
Udit <udit@192.168.2.11>
2015-12-09 09:06:47 +0800

    Welcome page styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,112 @@
-<form method="POST" action="/auth/login">
-    {!! csrf_field() !!}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
+    <style>
+        html, body {
+            height: 100%;
+        }
 
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
-    </div>
+        *{
+            box-sizing: border-box;
+        }
 
-    <div>
-        <button type="submit">Login</button>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 36px;
+            margin-bottom: 25px;
+        }
+
+        form{
+            padding: 50px 50px 25px;
+            min-width: 500px;
+            border: 1px solid #eee;
+        }
+
+        form > div{
+            margin-bottom: 25px;
+        }
+
+        input{
+            width: 100%;
+            padding: 8px 12px;
+            border-radius: 2px;
+            box-shadow: none;
+            font-size: 16px;
+            color: #444;
+            border: 2px solid #ccc;
+        }
+
+        label{
+            font-weight: bold;
+            display: block;
+            text-align: right;
+            margin-bottom: 8px;
+        }
+
+        button{
+            margin-top: 35px;
+            background: none;
+            border: 2px solid #ccc;
+            border-radius: 2px;
+            padding: 14px 25px;
+            min-width: 150px;
+            font-size: 16px;
+
+            transition: all 0.3s;
+        }
+
+        button:hover{
+            cursor: pointer;
+            background-color: #eee;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">Login to Fundator</div>
+
+        <form method="POST" action="/auth/login">
+            {!! csrf_field() !!}
+
+            <div>
+                <label for="email">Email</label>
+                <input id="email" type="email" name="email" value="{{ old('email') }}">
+            </div>
+
+            <div>
+                <label for="password">Password</label>
+                <input id="password" type="password" name="password" id="password">
+            </div>
+
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
     </div>
-</form>
+</div>
+</body>
+</html>
