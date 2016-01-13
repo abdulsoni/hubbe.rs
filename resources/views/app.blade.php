baa75a539f08a3006d3104c680acc3e540541508
Udit <udit@Udits-MacBook-Pro.local>
2016-01-14 01:23:11 +0800

    Add a meta key

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="Qb1ckt_q8K3bKQSW-JepzfbaUUbyZXHw-_BOP3nYQEU" />
     <title ng-bind="pageTitle()">Fundator</title>
 
     <link rel="stylesheet" href="{!! asset('css/app/app.css') !!}">
