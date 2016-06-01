879e7ee68af5b284b7791cf9747b00de0794a13a
Udit <udit@digitalcreative.asia>
2016-06-01 17:52:45 +0800

    Linkedin Fix

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -524,13 +524,15 @@ class AuthenticateController extends Controller
             if ($request->header('Authorization'))
             {
                 $linkedinProfile = LinkedinProfile::where('linkedin_id', $profile['id'])->first();
+                $linkedinProfileUser = User::find($linkedinProfile->user_id);
 
-                if (!is_null($linkedinProfile))
+                if (!is_null($linkedinProfile) && !is_null($linkedinProfileUser))
                 {
                     $linkedinProfile->linkedin_token = $accessToken['access_token'];
                     $linkedinProfile->save();
-                    $user = User::find($linkedinProfile->user_id);
-                    return response()->json(['token' => $user->getToken()], 200, [], JSON_NUMERIC_CHECK);
+                    return response()->json(['token' => $linkedinProfileUser->getToken()], 200, [], JSON_NUMERIC_CHECK);
+                }elseif (!is_null($linkedinProfile) && is_null($linkedinProfileUser)) {
+                    $linkedinProfile->delete();
                 }
 
                 $token = explode(' ', $request->header('Authorization'))[1];
