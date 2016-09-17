0f6b500a52fb87b3ad7360b5db86bd837053f71e
Udit <udit@digitalcreative.asia>
2016-09-17 17:41:44 +0800

    Homepage issues

---
+++ angular/directives/expandingItem.directive.js
@@ -0,0 +1,46 @@
+(function() {
+    "use strict";
+
+
+    angular.module('fundator.directives')
+
+    .directive('expandingItem', function($rootScope, $resource, $timeout, API) {
+        return {
+            templateUrl: '/views/app/app/partials/contest-item.html',
+            restrict: 'E',
+            priority: 999,
+            transclude: true,
+            scope: {
+                item: '='
+            },
+            link: function($scope, $element, $attrs) {
+                $scope.data = {};
+                $scope.item.thumbnail = '/images/contest.jpg';
+
+                var $parent = $element.parent();
+                var $item = $element.find('.item');
+                var $tabItems = $element.find('.item-tabs');
+
+                $element.on('click', function(){
+                    $element.addClass('active');
+
+                    var itemWidth = $item.width();
+                    var itemHeight = $item.outerHeight();
+
+                    $item.css('max-width', itemWidth + 'px');
+                    $item.css('width', itemWidth + 'px');
+
+                    // $tabItems.each(function(){
+                    //     $(this).find('li > a').css('height', ((itemHeight - 8) / 5) + 'px');
+                    //     console.log(((itemHeight - 8) / 5) + 'px');
+                    // });
+
+                    $timeout(function(){
+                        $parent.addClass('transitioning transitioning_expanding');
+                    });
+                });
+            }
+        };
+    });
+
+})();
